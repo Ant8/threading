a9ca7e794f83ad6dedc8c799f34a525d01571768
Antoni Moroz <antek@codequest.com>
2021-07-25 17:57:34 +0200

    added onclick handling through viewmodel

--- app/src/main/java/abm/ant8/threading/ui/main/MainFragment.kt
+++ app/src/main/java/abm/ant8/threading/ui/main/MainFragment.kt
@@ -1,32 +1,37 @@
 package abm.ant8.threading.ui.main
 
-import androidx.lifecycle.ViewModelProvider
+import abm.ant8.threading.databinding.MainFragmentBinding
+import android.Manifest
+import android.content.pm.PackageManager
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import abm.ant8.threading.R
-import abm.ant8.threading.databinding.MainFragmentBinding
 import androidx.activity.result.contract.ActivityResultContracts
-import androidx.databinding.DataBindingUtil
+import androidx.core.content.ContextCompat
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
 
 class MainFragment : Fragment() {
 
     companion object {
         fun newInstance() = MainFragment()
+
+        private const val REQUIRED_PERMISSION = Manifest.permission.ACCESS_COARSE_LOCATION
     }
 
     private lateinit var viewModel: MainViewModel
     private lateinit var dataBinding: MainFragmentBinding
 
-    val requestPermissionLauncher =
-    registerForActivityResult(
-        ActivityResultContracts.RequestPermission()
-    ) { isGranted: Boolean -> }
+    private val requestPermissionLauncher =
+        registerForActivityResult(
+            ActivityResultContracts.RequestPermission()
+        ) { isGranted: Boolean -> }
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
-                              savedInstanceState: Bundle?): View =
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View =
         MainFragmentBinding
             .inflate(inflater, container, false)
             .apply {
@@ -44,4 +49,17 @@ class MainFragment : Fragment() {
         // TODO: Use the ViewModel
     }
 
+    fun handleStartClicked(view: View) {
+        when {
+            ContextCompat.checkSelfPermission(
+                requireContext(),
+                REQUIRED_PERMISSION
+            ) == PackageManager.PERMISSION_GRANTED -> {
+                viewModel.checkThreadsPrerequisites()
+            }
+            else -> {
+                requestPermissionLauncher.launch(REQUIRED_PERMISSION)
+            }
+        }
+    }
 }
--- app/src/main/java/abm/ant8/threading/ui/main/MainViewModel.kt
+++ app/src/main/java/abm/ant8/threading/ui/main/MainViewModel.kt
@@ -1,7 +1,14 @@
 package abm.ant8.threading.ui.main
 
+import android.util.Log
 import androidx.lifecycle.ViewModel
 
 class MainViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+    fun checkThreadsPrerequisites() {
+        Log.d(TAG, "started threads")
+    }
+
+    companion object {
+        private const val TAG = "MainViewModel"
+    }
 }
