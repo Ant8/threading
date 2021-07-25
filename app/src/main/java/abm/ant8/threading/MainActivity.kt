7764d4baeee1d89cbac1a08f476ec0483acb0025
Antoni Moroz <antek@codequest.com>
2021-07-25 15:37:17 +0200

    chore: initial commit

---
+++ app/src/main/java/abm/ant8/threading/MainActivity.kt
@@ -0,0 +1,18 @@
+package abm.ant8.threading
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import abm.ant8.threading.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                    .replace(R.id.container, MainFragment.newInstance())
+                    .commitNow()
+        }
+    }
+}
---
+++ app/src/main/java/abm/ant8/threading/ui/main/MainViewModel.kt
@@ -0,0 +1,7 @@
+package abm.ant8.threading.ui.main
+
+import androidx.lifecycle.ViewModel
+
+class MainViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
